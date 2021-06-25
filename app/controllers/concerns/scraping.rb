7eef47c4e82931c874645743d1590a4bf91070de
rysk1013 <rysk2908@gmail.com>
2021-06-26 06:13:29 +0900

    create module Scraping

---
+++ app/controllers/concerns/scraping.rb
@@ -0,0 +1,26 @@
+module Scraping
+  require 'open-uri'
+  require 'nokogiri'
+
+  def set_scraping
+    url = 'https://coronavirus.smartnews.com/jp/'
+
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+
+    rows = {}
+
+    doc.xpath("//section[@class='detailSec jp-prefecture-list-container']").css('tr').each do |node|
+      rows[node.css('td[1]').text] = node.css('td[2]').text
+    end
+    
+    rows.delete("")
+
+    return rows
+  end
+end
